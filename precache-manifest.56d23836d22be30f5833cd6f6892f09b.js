635eef842a2db91ccb5497249c0b20eab04db736
Yibang Chen <ychen622@gmail.com>
2019-08-14 14:24:56 -0700

    Updates

---
+++ precache-manifest.56d23836d22be30f5833cd6f6892f09b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d72b5d1350e6c17d1671466bbcdced",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6f5cea43e2cb086c9a3a",
+    "url": "/home/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b716c37aa99ef40c1342",
+    "url": "/home/static/js/2.6f68412d.chunk.js"
+  },
+  {
+    "revision": "6f5cea43e2cb086c9a3a",
+    "url": "/home/static/js/main.a330ce91.chunk.js"
+  },
+  {
+    "revision": "100bee1ba02c73f2ad04",
+    "url": "/home/static/js/runtime~main.7cc2aec6.js"
+  }
+]);
